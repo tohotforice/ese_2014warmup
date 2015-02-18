7e72fcc6cb8ec80ef551d53b0c6b3cc011e72de4
Severin Zumbrunn <sz@tune-x.ch>
2015-02-18 13:03:55 +0100

    vfjshfhskj sghj gh DH-1

--- spring-mvc-archetype/src/main/java/MySpringMvc/di/CoreModule.java
+++ spring-mvc-archetype/src/main/java/MySpringMvc/di/CoreModule.java
@@ -6,18 +6,3 @@ import br.com.ingenieux.cloudy.awseb.di.BaseAWSModule;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-
-public class CoreModule extends AbstractModule {
-	@Provides
-	public ObjectMapper getObjectMapper() {
-		return new ObjectMapper();
-	}
-	
-    @Override
-    protected void configure() {
-    	install(new BaseAWSModule().withDynamicRegion());
-        // Uncomment to create beanstalk awareness
-        // install(new EC2Module());
-        // install(new BeanstalkEnvironmentModule());
-    }
-}
